45e2dc5fd3b09326dcf87adf96ec892f9426bc6f
silverframe <ibercombie@gmail.com>
2016-07-27 01:58:20 +0800

    Improved on reseting each habit's weekly target at the start of the week

--- exercise/Views/ListHabitTableViewCell.swift
+++ exercise/Views/ListHabitTableViewCell.swift
@@ -20,20 +20,18 @@ class ListHabitTableViewCell: UITableViewCell {
     
     @IBAction func habitFrequencyButtonClicked(sender: UIButton) {
         if let habit = habit {
+            //Changes the no of total completions, current streak, frequency
             habit.frequencyChange()
             self.habitFrequencyLabel.text = String(habit.habitFrequency)
-//            let date1 = NSDate()
             let date2 = habit.dateCompleted[0].date
             let calendar = NSCalendar.currentCalendar()
             calendar.timeZone = NSTimeZone.defaultTimeZone()
-//            let datesAreInTheSameDay = calendar.isDate(date1, equalToDate: date2, toUnitGranularity: [.Day, .Month, .Year])
             let datesAreInTheSameDay = calendar.isDateInToday(date2)
-//            let dateIstheDayBefore = calendar.isDateInYesterday(date2)
             if datesAreInTheSameDay != true {
                 sender.enabled = true
             } else {
                 sender.enabled = false
+                }
             }
         }
     }
-}
