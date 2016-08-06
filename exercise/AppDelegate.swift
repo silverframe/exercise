00200fc932d0c751395e0247303b378e6a59faba
silverframe <ibercombie@gmail.com>
2016-08-06 11:00:33 +0800

    Updated the function for reseting the current streak

--- exercise/AppDelegate.swift
+++ exercise/AppDelegate.swift
@@ -65,6 +65,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         Reset().checkReset()
+        Reset().streakReset()
         print("entered foreground")
         let notification = NSNotificationCenter.defaultCenter()
         notification.postNotificationName("EnterForeground", object: nil)
@@ -74,6 +75,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         print("became active")
         Reset().checkReset()
+        Reset().streakReset()
     }
 
     func applicationWillTerminate(application: UIApplication) {
--- exercise/reset.swift
+++ exercise/reset.swift
@@ -26,6 +26,23 @@ struct Reset {
         }
     }
     
+    func streakReset(){
+        let results = realm.objects(Habit)
+        for habit: Habit in results {
+            if habit.dateCompleted.count != 0 {
+                let date = habit.dateCompleted[0].date
+                let calendar = NSCalendar.currentCalendar()
+                calendar.timeZone = NSTimeZone.defaultTimeZone()
+                let dateInYesterday = calendar.isDateInYesterday(date)
+                if dateInYesterday != true {
+                    try! Realm().write {
+                        habit.currentStreak = 0 
+                    }
+                }
+            } 
+            }
+        }
+    
     func currentWeekValue() -> Int {
         let todayDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
